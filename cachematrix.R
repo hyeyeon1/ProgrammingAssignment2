f8765eb8b98fddf7ee6c02e269befde0ccc30523
hyeyeon1 <dshyeyeon@gmail.com>
2016-01-23 16:47:34 +0900

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
+## these functions are used to assign a value to an object in an environment that is different from the current environment.
+## it is useful to create a special object that stores a numeric vector and cache's its mean.
 
 ## this function makeVector creates a special "vector".
 
